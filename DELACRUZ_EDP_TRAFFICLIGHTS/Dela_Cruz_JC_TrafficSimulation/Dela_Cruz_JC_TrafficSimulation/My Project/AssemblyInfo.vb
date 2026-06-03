9af8ec43970088f783d84f255f08c7af2b11fc10
Jaice930 <djohncarlo44@gmail.com>
2026-06-03 21:06:43 +0800

    Add files via upload

---
+++ DELACRUZ_EDP_TRAFFICLIGHTS/Dela_Cruz_JC_TrafficSimulation/Dela_Cruz_JC_TrafficSimulation/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Dela_Cruz_JC_TrafficSimulation")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Dela_Cruz_JC_TrafficSimulation")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6ff4ec6f-803e-48b0-b153-23ea29ae07f0")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
